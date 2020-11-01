e739f9c56a4e1005cb9eb9608a4e6f2936c12fc7
Taylor Booth <taylorkbooth@gmail.com>
2020-11-01 15:18:58 -0500

    commit tree e50b6c1a9adcad776e09f671c7fe838c2de710cd

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+list = []
 
+
+
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
 
